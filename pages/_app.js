665e94e5da5880de3cf4483c2543d07bb733f985
Ryan Ceres <c.nizz@web.de>
2023-03-03 10:17:29 +0100

    add all necessary packages and configs

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import GlobalStyle from "@/styles";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Head>
+        <title>Capstone Project</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
